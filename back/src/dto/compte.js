71769fbb183ef36bb73fd6305774f4e7178b64c7
Alexandre-Chuzel <115239895+Alexandre-Chuzel@users.noreply.github.com>
2025-12-15 15:56:52 +0100

    Dto dossier, lien et compte
    Dto dossier, lien et compte

--- back/src/dto/compte.js
+++ back/src/dto/compte.js
@@ -1,12 +1,11 @@
-//Import de la variable de configuration
+// Import de la variable de configuration
 import { PG_CONFIG } from '../global_properties.js';
 import pgPromise from 'pg-promise';
 
 const pgp = pgPromise(/* initialization options */);
 const db = pgp(PG_CONFIG);
 
-// La class DTO pour le compte
-
+// La classe DTO pour le compte
 class DtoCompte {
     constructor(id, nom, solde) {
         this.id = id;
@@ -14,26 +13,27 @@ class DtoCompte {
         this.solde = solde;
     }
 
-    creerCompte(compte) {
-    // Simuler la sauvegarde du compte dans une base de données
-    const req = `INSERT INTO public."Compte" (nom, solde) VALUES ('${compte.nom}', ${compte.solde}) RETURNING *`;
-    let result = db.one(req);
-    return result;
+    async creerCompte(compte) {
+        try {
+            const req = `
+                INSERT INTO public.compte (nom, solde) 
+                VALUES ($1, $2) RETURNING *`;
+            return await db.one(req, [compte.nom, compte.solde]);
+        } catch (error) {
+            console.error('Erreur lors de la création du compte :', error);
+            throw error;
+        }
     }
 
     async recupererComptes() {
-    const req = 'SELECT * FROM public."Compte"';
-    return db.manyOrNone(req)
-        .then(result => {
-            return result;
-        })
-        .catch(error => {
-            console.error('Erreur lors de la récupération des comptes:', error);
+        try {
+            const req = 'SELECT * FROM public.compte';
+            return await db.manyOrNone(req);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des comptes :', error);
             throw error;
-        });
-}
-
-
+        }
+    }
 }
 
 export default DtoCompte;
