c8807a0ad4020f44d8ab55cd051ce9526acbf4b0
FOURNIER Cyprien <122486705+IRPYC37@users.noreply.github.com>
2025-12-15 15:14:50 +0100

    feat: add database creation and initialization logic in server setup

--- back/server.js
+++ back/server.js
@@ -1,6 +1,37 @@
 import express from 'express';
-import { PORT } from './src/global_properties.js';
+import { PORT, PG_CONFIG } from './src/global_properties.js';
 import compteRouter from './src/controller/compte.js';
+import { exec } from 'node:child_process';
+
+// Création de la database si elle n'existe pas
+exec(`PGPASSWORD=${PG_CONFIG.password} psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='${PG_CONFIG.database}'"`, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Erreur lors de la vérification de l'existence de la base de données: ${error.message}`);
+        return;
+    }
+    if (stdout.trim() !== '1') {
+        exec(`PGPASSWORD=${PG_CONFIG.password} psql -U postgres -c "CREATE DATABASE ${PG_CONFIG.database}"`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Erreur lors de la création de la base de données: ${error.message}`);
+                return;
+            }
+            console.log(`Base de données ${PG_CONFIG.database} créée avec succès.`);
+        });
+    }
+});
+
+// Execute le script SQL de création des tables
+exec(`PGPASSWORD=${PG_CONFIG.password} psql -U postgres -d ${PG_CONFIG.database} -f ./script.sql`, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Erreur lors de l'exécution du script SQL: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Erreur dans le script SQL: ${stderr}`);
+        return;
+    }
+    console.log(`Script SQL exécuté avec succès: ${stdout}`);
+});
 
 const app = express();
 
