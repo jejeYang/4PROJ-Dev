b7cfb174c402281ea185b60c1f73e0e8ccf09518
KilianMoreau <118806824+KilianMoreau@users.noreply.github.com>
2026-03-27 11:28:37 +0100

    version propre pluie de logo

--- front/src/components/FallingIcons.jsx
+++ front/src/components/FallingIcons.jsx
@@ -3,7 +3,7 @@ import './FallingIcons.css';
 import logo from '../assets/logo.png';
 
 const FallingIcons = () => {
-  const icons = Array.from({ length: 2000 }); // Un peu plus pour bien voir l'accumulation
+  const icons = Array.from({ length: 500 });
 
   return (
     <div className="falling-container">
@@ -15,7 +15,7 @@ const FallingIcons = () => {
           alt=""
           style={{
             left: `${Math.random() * 100}%`,
-            animationDelay: `${Math.random() * 1000}s`,
+            animationDelay: `${Math.random() * 100}s`,
             animationDuration: `${5 + Math.random() * 50}s`,
             opacity: 0.2 + Math.random() * 0.4,
             width: `${15 + Math.random() * 30}px`
